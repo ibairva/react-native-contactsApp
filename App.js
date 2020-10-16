687bdc263a48d1b39832ac3e43d617b8af96b231
Indrajeet Bairva <ibairva@gmail.com>
2020-10-16 20:06:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,78 @@
+import 'react-native-gesture-handler';
+import React, {Component} from 'react';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import myReducer from "./src/reducers/myReducer";
+import thunk from 'redux-thunk';
+// import createSagaMiddleware from 'redux-saga';
+// import { watchGet } from './src/sagas/saga';
+import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Header from './src/components/header';
+import GetDataHome from './src/components/GetDataHome';
+import PostScreen from './src/components/PostScreen';
+import { Icon } from 'react-native-elements';
+
+
+
+// const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(myReducer, applyMiddleware(thunk));
+
+// const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// sagaMiddleware.run(watchGet);
+
+// function MyStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Home" component={GetDataHome} />
+//       <Stack.Screen name="AddContact" component={PostScreen} />
+//     </Stack.Navigator>
+//   );
+// }
+function MyTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen 
+        name="Home" 
+        component={GetDataHome} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='home' color={color} size={size} />
+          ),
+        }} />
+
+      <Tab.Screen 
+        name="AddContact" 
+        component={PostScreen}
+        options={{
+        tabBarIcon: ({ color, size }) => (
+          <Icon name='contacts' color={color} size={size} />
+        ),
+      }} />
+    </Tab.Navigator>
+  );
+}
+
+
+class App extends Component {
+
+    render(){     
+      return (
+      <>
+      <Provider store={store}>
+      <Header />
+          <NavigationContainer>
+            {/* <MyStack /> */}
+            <MyTab />
+          </NavigationContainer>   
+      </Provider>
+    </>
+      );
+    }
+}
+export default App;
+
