87d8409ae88c2c2f6372ecf3167f64f6e638dbc7
Indrajeet Bairva <ibairva@gmail.com>
2020-10-17 09:20:44 +0530

    Update contactList.js

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = (state) => {
-    console.log("hello",state);
+    // console.log("hello",state);
     return {
       contacts: state,
     };
