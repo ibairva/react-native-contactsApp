ba6ee05bdf5d9296ca135b1665e891923f8bb20b
Indrajeet Bairva <ibairva@gmail.com>
2020-10-17 09:17:21 +0530

    Update myAction.js

--- src/actions/myAction.js
+++ src/actions/myAction.js
@@ -1,46 +1,19 @@
-import axios from 'axios';
-
-export function getData()
-   {
-    return async (dispatch) => {
-    let resp = await axios.get('https://5f8304fc6b97440016f4e219.mockapi.io/contacts')
-    const myData = resp.data.items;
-    console.log(myData);
-    dispatch({
-                type: "GET_DATA",
-                payload: myData,
-              });
-   };
-  }
+export const GET_DATA = "GET_DATA";
+export const RECEIVE_API_DATA = "RECEIVE_API_DATA";
 
 
+export const DELETE_DATA = "DELETE_DATA";
+export const RECEIVE_DELETED_DATA = "RECEIVE_DELETED_DATA";
 
-  export function postData(theName,thePhone)
-   {     
-    return async (dispatch) => {
-     let resp = await axios.post('https://5f8304fc6b97440016f4e219.mockapi.io/contacts',{
-      name:theName,
-      phone:thePhone
-    })
-    alert('Contact Added Successfully');
-    console.log(resp.data);
-    dispatch({
-      type: "POST_DATA",
-      payload: resp.data
-    });
-  };
-}
 
- export function deleteData(item)
-  {
-    return async (dispatch) => {
-  let resp = await axios.delete('https://5f8304fc6b97440016f4e219.mockapi.io/contacts/'+item);
-   console.log(resp.data);
-   alert('Contact Deleted Successfully');
-   dispatch({
-    type: "DELETE_DATA",
-    payload: item
-  });
-  };
-}
+export const POST_DATA = "POST_DATA";
+export const RECEIVE_POSTED_DATA = "RECEIVE_POSTED_DATA";
 
+export const getData = () => ({ type: GET_DATA});
+export const receiveApiData = payload => ({ type: RECEIVE_API_DATA, payload });
+
+export const deleteData = (id) => ({ type: DELETE_DATA, id});
+export const receiveDeletedData = payload => ({ type: RECEIVE_DELETED_DATA, payload });
+
+export const postData = (name, phone) => ({ type: POST_DATA, name, phone });
+export const receivePostedData = payload => ({ type: RECEIVE_POSTED_DATA, payload });
